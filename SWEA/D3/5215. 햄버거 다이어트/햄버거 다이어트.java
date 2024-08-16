e1d090e9f66ffbdcc4e26f138ff52d96919c26ce
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2024-08-16 13:32:18 +0900

    [D3] Title: 햄버거 다이어트, Time: 277 ms, Memory: 20,320 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,23 +1,21 @@
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 조합으로 칼로리 수가 제한 칼로리보다 작은 모든 경우의 수를 구하고,
-// 점수의 최대값을 갱신한다.
-
-public class Solution{
+public class Solution {
 	private static int[][] arr;
+	private static boolean[] isSelected;
 	private static int N;
 	private static int L;
 	private static int ans;
-	
 	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("./res/d0814/input_SWEA_5215"));
+		 //System.setIn(new FileInputStream("res/d0816/input_SWEA_5215"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		int T = Integer.parseInt(br.readLine()); // Test Case
-		
+		int T = Integer.parseInt(br.readLine());
 		for(int tc=1; tc<=T; tc++) {
 			ans = 0;
 			st = new StringTokenizer(br.readLine());
@@ -33,20 +31,39 @@ public class Solution{
 				arr[i][1] = calorie; // 칼로리
 			}
 			
-			combination(0, 0, 0);
+			//combination(0, 0, 0);
+			powerSet(0,0, 0, new boolean[N]);
 			System.out.printf("#%d %d",tc,ans);
 			System.out.println("");
 			
 		}
 	}
-		public static void combination(int startIndex, int scores, int calories) {
+	
+		public static void combination(int startIndex, int scores, int calories) { // 조합
 			if(calories <= L) { // 칼로리의 합이 최대 칼로리보다 작으면
 				ans=Math.max(scores, ans); // 최대값 갱신
 			}
-			for(int i=startIndex; i<N; i++) { // startIndex부터 차례대로 더하기
-				int calorie = calories+ arr[i][1];
-				int score = scores+ arr[i][0];						
-				combination(i+1, score, calorie);
+			for(int i=startIndex; i<N; i++) { // startIndex부터 차례대로 더하기						
+				combination(i+1, scores+arr[i][0], calories+arr[i][1]);
 			}
-	}
+		}
+		
+		public static void powerSet(int cnt, int scores, int calories, boolean[] isSelected) { // 부분집합
+			if (calories <= L) { //최대 칼로리보다 작을 때
+				ans=Math.max(scores, ans);
+				
+			}
+			if(cnt == N) { // 모든 재료의 수를 다 썼으면
+				return;
+			}
+			else {
+				//System.out.println(Arrays.toString(isSelected));
+				isSelected[cnt] = true;
+				powerSet(cnt+1,  scores+arr[cnt][0], calories+arr[cnt][1], isSelected);
+				isSelected[cnt] = false;
+				powerSet(cnt+1, scores, calories, isSelected);
+				
+				
+			}
+		}
 }
