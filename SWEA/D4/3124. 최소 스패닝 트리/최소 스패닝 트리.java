4f40cef8a9bab07822be0559d89efd1b9b920079
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2024-08-30 12:11:35 +0900

    [D4] Title: 최소 스패닝 트리, Time: 4,863 ms, Memory: 241,204 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
-	/* 
+	/*
 	 * [문제 풀이를 위한 아이디어] 
-	 * 입력을 받을 때, 간선으로 받는다
-	 * Edge클래스를 만들어서 start, end, weight를 받는다.
-	 * 클래스 내에서 compareTo를 재정의한여 weight를 오름차순 정렬한다.
-	 * (최소 가중치를 만들어야 하기 때문에 가장 낮은 weight를 가진 간선부터 선택해야 하기 때문이다.)
-	 * 
-	 * a와 b를 union 연산하고, 연산이 되면 서로의 부모 노드가 다르다는 것이니
-	 * 선택한 간선의 수 + 1, cost에 weight를 누적한다.
-	 * 선택한 간선의 수가 노드개수 -1, 즉 V-1이 되면 멈춘다.
+	 * 1. MST -> 프림
+	 * 2. 인접 행렬을 우선순위 큐가 담긴 리스트로 초기화 
+	 * 3. 우선순위 큐는 cost가 작은 순서대로 정렬되어 있음
+	 * 4. 임의의 점부터 시작하여 해당 정점의 간선들을 모두 pq에 넣음
+	 * 5. pq에서 차례대로 꺼내서 방문하지 않았다면 방문처리, 사용한 간선 수 증가
+	 * -> 해당 노드와 연결된 최소 비용 간선을 선택한 것임
+	 * 6. 이를 사용한 간선이 V-1개가 될 때까지 반복함
 	 * 
 	 */
-	
-	
+
 	static int V;
 	static int E;
-	static int[] parents;
+	static boolean[] visited;
+	static PriorityQueue<Edge> pq;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,74 +30,63 @@ public class Solution {
 			V = Integer.parseInt(st.nextToken()); // 정점의 개수
 			E = Integer.parseInt(st.nextToken()); // 간선의 개수
 
-			Edge[] edges = new Edge[E]; // 간선들
+			PriorityQueue<Edge>[] adjList = new PriorityQueue[V + 1];
+
+			// 인접 행렬 초기화
+			for (int i = 1; i <= V; i++) {
+				adjList[i] = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost,  o2.cost));
+			}
 
 			for (int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken()); // 노드 a
-				int b = Integer.parseInt(st.nextToken()); // 노드 b
-				int c = Integer.parseInt(st.nextToken()); // 가중치 c
-				edges[i] = new Edge(a, b, c); // a, b 사이의 가중치 c를 갖는 간선
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+
+				adjList[a].offer(new Edge(b, c));
+				adjList[b].offer(new Edge(a, c));
 			}
 
-			Arrays.sort(edges); // 최소 스패닝을 위해 weight 기준 오름차순 정렬
-			make();
-			
-			long count = 0, cost = 0;
-			for (Edge e : edges) { // 간선들 차례로 돌며 union
-				if (union(e.a, e.b)) {
-					count++; // 선택한 간선의 수
-					cost += e.weight; // 간선의 가중치 합
-					if (count == V - 1)
-						break; // 간선의 수가 N-1 되면 종료
-				}
+			// [ 프림 알고리즘 ]
+			// 초기화
+			visited = new boolean[V + 1];
+			pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost,  o2.cost));
+
+			// 임의의 점부터 시작, 1부터
+			visited[1] = true;
+			pq.addAll(adjList[1]);
+
+			long ans = 0;
+			int edgesUsed = 0;
+
+			while (!pq.isEmpty() && edgesUsed < V - 1) {
+				Edge current = pq.poll();
+
+				if (visited[current.to])
+					continue; // 방문했던 정점 무시
+
+				// 최소 스패닝 트리에 정점 추가
+				visited[current.to] = true;
+				ans += current.cost;
+				edgesUsed++;
+
+				// pq에 이 정점과 인접한 모든 간선들을 추가
+				pq.addAll(adjList[current.to]);
+
 			}
-			System.out.printf("#%d %d\n", tc, cost);
-		}
 
-	}
-
-	static void make() { // 자신을 부모 노드로 설정
-		parents = new int[V + 1];
-		for (int i = 1; i <= V; i++) {
-			parents[i] = -1;
+			System.out.printf("#%d %d\n", tc, ans);
 		}
 	}
 
-	static int findSet(int a) { // 루트 노드 찾음
-		if (parents[a] < 0)
-			return a;
-		return parents[a] = findSet(parents[a]);
-	}
+	static class Edge{
+		int to, cost;
 
-	static boolean union(int a, int b) { // 합집합
-		int aRoot = findSet(a);
-		int bRoot = findSet(b);
-
-		if (aRoot == bRoot)
-			return false; // 부모 노드가 같으면 합칠 수 없음
-
-		parents[aRoot] += parents[bRoot]; // aRoot를 부모 노드로 하여 합침
-		parents[bRoot] = aRoot;
-		return true;
-	}
-
-	static class Edge implements Comparable<Edge> { // 간선 클래스
-		int a;
-		int b;
-		int weight;
-
-		public Edge(int a, int b, int weight) {
+		public Edge(int to, int cost) {
 			super();
-			this.a = a;
-			this.b = b;
-			this.weight = weight;
+			this.to = to;
+			this.cost = cost;
 		}
-
-		@Override
-		public int compareTo(Edge o) { // weight 오름차순
-			return Integer.compare(this.weight, o.weight);
-		}
-
 	}
+
 }
