5ac801368306e4d89a8452ec6ae3d161747dccdb
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2024-09-03 17:38:17 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 300 ms, Memory: 28,256 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -5,6 +5,11 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
+	
+	/*
+	 * 가중치 양수, 최단 거리, 다익스트라 알고리즘
+	 * 
+	 */
 	static int N;
 	static int[][] map;
 	static int[] dr = {-1, 1, 0, 0};
@@ -17,17 +22,15 @@ public class Solution {
 			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
 			for(int i=0; i<N; i++) {
-				//StringTokenizer st = new StringTokenizer(br.readLine());
 				char[] ch = br.readLine().toCharArray();
 				for(int j=0; j<N; j++) {
-					//map[i][j] = Integer.parseInt(st.nextToken());
 					map[i][j] = ch[j] - '0';
 				}
 			}
-			
 			System.out.println("#"+tc+" "+getMinTime(0,0,N-1, N-1));
 		}
 	}
+	
 	static int getMinTime(int sr, int sc, int er, int ec) {
 		final int INF = Integer.MAX_VALUE;
 		boolean[][] visited = new boolean[N][N];
@@ -35,11 +38,14 @@ public class Solution {
 		PriorityQueue<int[]> pQueue = new PriorityQueue<>((o1, o2)-> o1[2]-o2[2]);
 		// int[] = {r, c, time}, time기준으로 최소 힙 관리
 		
+		// minTime 초기화
 		for(int i=0; i<N; i++) {
 			for(int j =0; j<N; j++) {
 				minTime[i][j] = INF;
 			}
 		}
+		
+		// 첫 정점 0
 		minTime[sr][sc] = 0;
 		pQueue.offer(new int[] {sr, sc, minTime[sr][sc]});
 		
@@ -49,22 +55,25 @@ public class Solution {
 			int c = stopOver[1];
 			int time = stopOver[2];
 			
+			// 이미 방문했던 노드 X - 이미 최소값임
 			if(visited[r][c]) continue;
 			visited[r][c] = true;
 			
 			if(r==er && c == ec) return time;
 			
-			// step2. 현재 정점 기준으로 사방 탐색
+			// 현재 정점 기준으로 사방 탐색
 			for(int d = 0; d< 4; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
-				if(nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc] && minTime[nr][nc] > time+map[nr][nc]) {
+				if(isInBound(nr, nc)&& !visited[nr][nc] && minTime[nr][nc] > time+map[nr][nc]) {
 					minTime[nr][nc] = time + map[nr][nc];
 					pQueue.offer(new int[] {nr, nc, minTime[nr][nc]});
-					
 				}
 			}
 		}
 		return -1;
+	}
+	public static boolean isInBound(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < N;
 	}
 }
