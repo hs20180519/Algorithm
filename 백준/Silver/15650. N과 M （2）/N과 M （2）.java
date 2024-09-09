78405016d31c097c57429b1cfb728ac2d8a594b8
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2024-09-09 13:34:47 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -5,32 +5,28 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int M;
-    private static int N;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 자연수 N 중
-        M = Integer.parseInt(st.nextToken()); // M개를 순서 없이 뽑음
-
-        combination(0, 1, new int[M]);
-
-    }
-    // cnt : answers 에 들어간 수
-    // startNumber: 시작 수 (1 ~ N)
-    // answers[] : 조합을 저장할 배열
-    private static void combination(int cnt, int startNumber, int []answers){
-        if (cnt== M){
-            for(int i=0; i<M; i++){
-                System.out.printf("%d ", answers[i]);
-            }
-            System.out.println();
-            return;
-        }
-        for(int i=startNumber; i<=N; i++){
-            answers[cnt] = i;
-            combination(cnt+1, i+1, answers);
-        }
-    }
+	/*
+	 * 중복 없이 M개를 고른 조합
+	 */
+	static int N, M;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		combination(0,1, new int[M]);
+	}
+	public static void combination(int cnt, int startIndex, int[] answers) {
+		if(cnt == M) {
+			for(int i=0; i<cnt; i++) {
+				System.out.print(answers[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for(int i=startIndex; i<=N; i++) {
+			answers[cnt] = i;
+			combination(cnt+1, i+1, answers);
+		}
+	}
 }
