706e0978bcce33a7f86d0e4b8b589ae3e57ce8f5
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2024-11-04 11:52:35 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 196 ms, Memory: 17068 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -2,22 +2,29 @@
 import java.io.*;
 import java.util.*;
 
+
+// bfs 시간초과
+// 플로이드-워샬 (N^3)
+
 public class Main {
 	static int N;
-	static ArrayList<Integer>[] graph;
+	static int[][] distances;
 	static int minDistance = Integer.MAX_VALUE;
-	static int x = Integer.MAX_VALUE;
-	static int y = Integer.MAX_VALUE;
-
+	static int x = -1;
+	static int y = -1;
+	static int INF = 1000000;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken()); // 건물의 개수 (정점)
 		int M = Integer.parseInt(st.nextToken()); // 도로의 개수 (간선)
-		graph = new ArrayList[N + 1];
 
-		for (int i = 0; i < N + 1; i++) {
-			graph[i] = new ArrayList<>();
+		// 거리 배열 초기화
+		distances = new int[N+1][N+1];
+		for (int i = 1; i < N + 1; i++) {
+			Arrays.fill(distances[i], INF);
+			distances[i][i] = 0; // 자기 자신 0
 		}
 
 		for (int i = 0; i < M; i++) {
@@ -25,21 +32,28 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			graph[a].add(b);
-			graph[b].add(a);
+			distances[a][b] = 1;
+			distances[b][a] = 1;
 		}
 
+		// 최단 거리 계산
+		flodyWarshall();
+		
 		 // 두 개 조합 선택
 		 chooseTwo(0, 1, new int[2]);
 		
 		 System.out.println(x+" " + y + " " +minDistance);
-//		int[] ans = {1, 2};
-//		
-//		for(int i=1; i<N+1; i++) {
-//			boolean[] visited = new boolean[N+1];
-//			System.out.println(bfs(i,visited, 1, 2));
-//		}
 
+	}
+
+	private static void flodyWarshall() {
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N;i++) {
+				for(int j=1; j<=N; j++) {
+					distances[i][j] = Math.min(distances[i][j], distances[i][k] + distances[k][j]); 
+				}
+			}
+		}	
 	}
 
 	public static void chooseTwo(int cnt, int startIndex, int[] answers) {
@@ -47,20 +61,14 @@ public class Main {
 			// 모든 도시에서의 왕복 시간의 합 구하기
 			int currentDistance = caculateAllDistance(answers);
 		
-			if(minDistance > currentDistance) {
+			// 최단거리가 같다면
+			// 건물 번호 중 작은게 더 작을 수록, 작은 번호가 같다면 더 큰 번호가 작을 수록
+			if(currentDistance < minDistance || 
+					(currentDistance == minDistance && (answers[0] < x || (answers[0] == x && answers[1] < y)))) {
 				minDistance = currentDistance;
 				x = answers[0];
 				y = answers[1];
 			}
-			else if(minDistance == currentDistance) {
-				if(x > answers[0]) {
-					x = answers[0];
-					y = answers[1];
-				}else if(x == answers[0]) {
-					y = Integer.min(answers[1], y);
-				}
-			}
-			// System.out.println(x + " " + y);
 			return;
 		}
 
@@ -76,41 +84,11 @@ public class Main {
 		int dist = 0;
 	
 		for (int i = 1; i < N + 1; i++) { // 1번부터 치킨집 위치까지의 최단 거리 찾음
-			boolean[] visited = new boolean[N+1];
-			// System.out.println(bfs(i, visited, a, b));
-			dist += bfs(i,visited,a, b);
-		}
-		// System.out.println("토탈거리" + dist);
-		return dist;
-	}
-	public static int bfs(int v, boolean[] visited, int a, int b) {
-		Queue<int[]> q = new LinkedList<>();
-		visited[v] = true; // 방문
-		
-		q.add(new int[] {v, 0});
-		if(v == a || v == b) {
-			return 0;
-		}
-		
-		while(!q.isEmpty()) {
-			int[] curr = q.poll();
-			int c = curr[0];
-			int dist = curr[1]+1;
-			
-			for(int k : graph[c]) {
-				if(k == a || k == b) {
-					// 종료
-					return dist*2;
-				}
-				
-				if(!visited[k]) {
-					visited[k] = true;
-					q.add(new int[] {k, dist+1});
-				}
+			if (i != a && i != b) {
+				dist += Math.min(distances[i][a], distances[i][b]) * 2;
 			}
 		}
-		return -1;
+		return dist;
 	}
-	
 
 }
