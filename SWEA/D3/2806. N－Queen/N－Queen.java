da473ae7d61e43cbda9a8f2eb363a756d878a75b
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2024-11-25 15:48:52 +0900

    [D3] Title: N-Queen, Time: 114 ms, Memory: 20,192 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,49 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-	/*
-	 * 서로 다른 두 퀸이 공격하지 못하게 놓는 경우의 수
-	 */
-	static int N;
-	static int[] selected;
+	static int ans;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc<=T; tc++) {
-			N = Integer.parseInt(br.readLine()); // 보드 크기
-			selected = new int[N]; 
-			//System.out.println(dfs(0));
-			System.out.println("#"+tc+" "+dfs(0));
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] map = new int[N][N];
+			ans = 0;
+			sb.append("#").append(tc).append(" ");
+			dfs(0, new int[N], N);
+			sb.append(ans).append("\n");
 		}
+		System.out.println(sb);
 	}
-	public static int dfs(int index) {
-		if (index == N) {
-			return 1;
+
+	public static void dfs(int cnt, int[] selected, int N) {
+		if(cnt == N) {
+			ans++;
+			return;
 		}
 		
-		int ret = 0;
 		for(int i=0; i<N; i++) {
-			selected[index] = i;
-			if(possible(index)) {
-				ret += dfs(index+1);
+			selected[cnt] = i;
+			if(possible(cnt, selected)) {
+				dfs(cnt+1, selected, N);
 			}
 		}
-		return ret;
 	}
-	public static boolean possible(int index) {
-		for(int i=0; i<index; i++) {
-			// 이미 다른 곳에서 선택된 행
-			if(selected[i] == selected[index]) {
-				return false;
-			}
-			// 대각선 상에 놓여있는 경우
-			if(Math.abs(index-i) == Math.abs(selected[index]-selected[i])) {
-				return false;
-			}
+	
+	public static boolean possible(int cnt, int[] selected) {
+		for(int i=0; i<cnt; i++) {
+			if(selected[cnt] == selected[i]) return false;
 			
+			if(Math.abs(cnt-i) == Math.abs(selected[cnt]-selected[i])) return false;
 		}
 		return true;
 	}
+
 }
