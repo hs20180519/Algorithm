abe19f5069cac0ae9eb4755038b4c525d16c431c
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2024-12-08 16:45:46 +0900

    [Silver I] Title: 회전 초밥, Time: 164 ms, Memory: 18260 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,76 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[] fishes;
-    static boolean[] visited;
-    static int d, k;
-    static int N;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        // 가능한 다양한 종류의 초밥
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); // 접시 수
+		int d = Integer.parseInt(st.nextToken()); // 초밥 가지 수
+		int k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시의 수 (슬라이딩 윈도우 크기)
+		int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
+		int[] chobab = new int[N];
+		int[] eaten = new int[d+1];
+		eaten[c] = 3001; // 최대값
+		int count = 1;
 
-        N = Integer.parseInt(st.nextToken()); // 접시의 수
-        d = Integer.parseInt(st.nextToken()); // 초밥의 가짓수
-        k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시의 수
-        int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
+		for(int i=0; i<N; i++){
+			chobab[i] = Integer.parseInt(br.readLine());
+		}
 
-        fishes = new int[N];
-        for (int i = 0; i < N; i++) {
-            fishes[i] = Integer.parseInt(br.readLine());
-        }
+		for(int i=0; i<k; i++){
+			int e = chobab[i];
+			if(eaten[e] == 0) count++;
+			eaten[e]++;
+		}
 
-        int maxNum = 0;
-        for (int start = 0; start < N; start++) {
+		int max = count;
+		for(int i=0; i<N-1; i++){
+			int start = chobab[i];
+			int end = chobab[i+k<N ? i+k : (i+k) % N];
 
-            int end = start + k <= N ? start + k - 1 : start + k - N - 1;
-            // System.out.println(start + "," + end);
-            maxNum = Math.max(countFishes(start, end, c), maxNum);
+			if(--eaten[start] == 0) count--;
+			if(++eaten[end] == 1) count++;
+			max = Math.max(max, count);
+		}
 
-        }
+		System.out.println(max);
 
-        // start가 4일때 4,5,6,7 => end는 7 (if start + k(8) <= N) end = start+k-1,(7)
-        // start가 5일대 5,6,7,0 => end는 0 (if start + k(9) > N) end = start+k - N -1 (0)
-
-
-        // 5,6,7,0 / 6,7,0,1 / 7, 0, 1, 6 의 경우들도 세야 한다 !!!
-        // end가 start보다 작을 때 케이스 추가하기
-
-        System.out.println(maxNum);
-
-    }
-
-    static int countFishes(int start, int end, int coupon) {
-        visited = new boolean[d+1];
-        int count = 0;
-        if (start < end) {
-            for (int i = start; i <= end; i++) {
-                if (!visited[fishes[i]]) {
-                    count++;
-                    visited[fishes[i]] = true;
-                }
-            }
-        } else {
-            for (int i = start; i < N; i++) {
-                if (!visited[fishes[i]]) {
-                    count++;
-                    visited[fishes[i]] = true;
-                }
-            }
-
-            for (int j = 0; j <= end; j++) {
-                if (!visited[fishes[j]]) {
-                    count++;
-                    visited[fishes[j]] = true;
-                }
-            }
-        }
-        if(!visited[coupon]) count++;
-        return count;
-    }
+	}
 }
-
