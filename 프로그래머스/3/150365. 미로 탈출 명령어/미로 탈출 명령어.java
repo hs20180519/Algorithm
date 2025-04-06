360322ebb9ecf2a5e9411aa0b35ee51b28d0ed25
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-04-06 11:03:26 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.36 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -2,42 +2,40 @@ class Solution {
     // down, left, right, up
     int[] dx = {1, 0, 0, -1};
     int[] dy = {0, -1, 1, 0};
+    char[] dir = {'d', 'l', 'r', 'u'};
     boolean isFound = false;
     StringBuilder sb = new StringBuilder();
+    String answer = "impossible";
+    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "";
         dfs(0, x, y, r, c, k, n, m);
         
-        if(isFound) System.out.println(sb.toString());
-        else{
-            System.out.println("IMPOSSIBLE");
-        }
         return answer;
     }
     
     void dfs(int depth, int x, int y, int r, int c, int k, int n, int m){
         if(isFound) return;
         
-        
         int distance = Math.abs(x - r) + Math.abs(y - c);
         int remain = k - depth;
+
 
         // 가지치기 (불가능한 경로 제거)
         if (distance > remain || (remain - distance) % 2 != 0) return;
         
         if(depth == k){
             if(x == r && y == c){
-                // System.out.println(sb.toString());
                 isFound = true;
-                return;
+                answer = sb.toString();
             }
+            return;
         }
         
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
             if(isIn(nx, ny, n, m)){
-                sb.append(i);
+                sb.append(dir[i]);
                 dfs(depth+1, nx, ny, r, c, k, n, m); 
                 sb.deleteCharAt(sb.length() - 1);
                 if(isFound) return;
@@ -45,6 +43,6 @@ class Solution {
         }
     }
     boolean isIn(int x, int y, int n, int m){
-        return 0<=x && x < n && 0<= y && y < m;
+        return 1<=x && x <= n && 1<= y && y <= m;
     }
 }
