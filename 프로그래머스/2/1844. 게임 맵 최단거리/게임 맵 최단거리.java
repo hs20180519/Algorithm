c3eea23ddb1e650fffc523ac78d4ece84c99c5c2
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-04-29 16:59:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.13 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,48 @@
 import java.util.*;
 import java.io.*;
 class Solution {
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
-    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    boolean[][] visited;
     public int solution(int[][] maps) {
-   
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        int[][] visited = new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                visited[i][j] = Integer.MAX_VALUE;
-            }
-        }
     
-        int answer = bfs(maps, visited, n, m);
+        int a = maps.length;
+        int b = maps[0].length;
+        visited = new boolean[a][b];
+        
+        int answer = bfs(0, 0, a, b, maps);
         return answer;
     }
     
-    int bfs(int[][] maps, int[][] visited, int n, int m){
+    public int bfs(int startX, int startY, int a, int b, int[][] maps){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
-        visited[0][0] = 1;
+        q.add(new int[]{startX, startY, 0});
+        visited[startX][startY] = true;
+        
         while(!q.isEmpty()){
-            int[] curr= q.poll();
-            int x = curr[0];
-            int y = curr[1];
+            int[] c = q.poll();
+            int cx = c[0];
+            int cy = c[1];
+            int dist = c[2];
             
-            
-            if(visited[n-1][m-1]!=Integer.MAX_VALUE){
-                return visited[n-1][m-1];
-            }
+            if(cx == a-1 && cy == b-1)
+                return dist+1;
             
             for(int d=0; d<4; d++){
-                int nx = x+dx[d];
-                int ny = y+dy[d];
-                
-                if(0<=nx && nx < n && 0<=ny && ny<m && maps[nx][ny] == 1 && visited[x][y]+1 < visited[nx][ny]){
-                    q.add(new int[]{nx, ny});
-                    visited[nx][ny] = visited[x][y]+1;
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+                if(inValid(nx, ny, a, b)){
+                    if(maps[nx][ny] == 1){
+                        visited[nx][ny] = true;
+                        q.add(new int[]{nx, ny, dist+1});
+                    }
                 }
             }
         }
         return -1;
+    }
+    
+    public boolean inValid(int x, int y, int a, int b){
+        return 0 <= x && x < a && 0 <= y && y < b && (!visited[x][y]);
     }
 }
