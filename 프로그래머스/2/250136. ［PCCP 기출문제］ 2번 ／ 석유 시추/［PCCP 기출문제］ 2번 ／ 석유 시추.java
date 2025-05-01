9d9c03f23cb149d3d7b839b92cedf6aba78559b9
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-05-01 23:32:54 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 136.89 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,83 +1,81 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.TreeSet;
+import java.util.*;
+class Point{
+    int landNumber;
+    int landSize;
+    
+    public Point(int landNumber, int landSize){
+        this.landNumber = landNumber;
+        this.landSize = landSize;
+    }
+}
 
-public class Solution {
-//	public static void main(String[] args) {
-//		int[][] land = new int[][] { { 0, 0, 0, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1, 1, 0, 0 }, { 1, 1, 0, 0, 0, 1, 1, 0 },
-//				{ 1, 1, 1, 0, 0, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0, 1, 1 } };
-//		System.out.println(solution(land));
-//	}
-
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static boolean[][] visited;
-	static int[] landSize;
-	
-	public static int solution(int[][] land) {
-		int answer = 0;
-		int n = land[0].length; // 가로 8
-		int m = land.length; // 세로 5
-		
-		// bfs를 돌아 땅 번호 저장
-		// 개수는 따로 기록해두기
-	
-		visited = new boolean[m][n];
-		int landNumber = 1;
-		landSize = new int[(n*m)/2+1];
-		
-		// 땅 번호 기록
-		for(int i=0; i<m; i++) {
-			for(int j=0; j<n; j++) {
-				if(land[i][j] == 1 && !visited[i][j]) {
-					// 땅 번호 변경 및 땅 크기 저장
-					landSize[landNumber] = bfs(i, j, land, landNumber,n, m);
-					landNumber++;
-				}
-			}
-		}
-		
-
-		// 0열 ~ n열까지 비교
-		// 같은 landNumber면 더하지 않는다. (set 사용)
-		for(int i=0; i<n; i++) {
-			int temp = 0;
-			TreeSet<Integer> s = new TreeSet<>(); // 땅 번호를 저장할 트리셋
-			for(int j=0; j<m; j++) {
-				if(land[j][i] !=0 && !s.contains(land[j][i])) { // 0이 아니고 이미 셋에 없다면
-					s.add(land[j][i]);
-					temp += landSize[land[j][i]];
-				}
-			}
-			answer = Math.max(temp, answer);
-		}
-		
-		return answer;
-	}
-	
-	public static int bfs(int x, int y, int[][] land, int landNumber, int n, int m) {
-		Queue <int[]> q = new LinkedList<>();
-		q.add(new int[] {x,y});
-		visited[x][y] = true;
-		land[x][y] = landNumber;
-		int count = 1; // 석유 덩어리 크기
-		
-		while(!q.isEmpty()) {
-			int[] curr = q.poll();
-			int cx = curr[0];
-			int cy = curr[1];
-			for(int d=0; d<4; d++) {
-				int nx = cx + dx[d];
-				int ny = cy + dy[d];
-				if(0<=nx && nx<m && 0<=ny && ny<n && !visited[nx][ny] && land[nx][ny]== 1) {
-					q.add(new int[] {nx, ny});
-					visited[nx][ny] = true;
-					land[nx][ny] = landNumber;
-					count++;
-				}
-			}
-		}
-		return count;
-	}
-
+class Solution {
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    boolean[][] visited;
+    Point[][] newLand;
+    
+    public int solution(int[][] land) {
+        int r = land.length;
+        int c = land[0].length;
+        visited = new boolean[r][c];
+        this.newLand = new Point[r][c];
+        // 1. 맵에 bfs를 돌려서 각각의 (땅 번호와 석유덩어리 크기)를 저장함
+    
+        int number = 1;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(land[i][j] == 1 && !visited[i][j]){
+                    bfs(land, i, j, number++);
+                }
+            }
+        }
+        int answer = 0;
+        for(int i=0; i<c; i++){
+            Set<Integer> s = new HashSet<>();
+            int tempSum = 0;
+            for(int j=0; j<r; j++){
+                Point p = newLand[j][i];
+                if (p != null && !s.contains(p.landNumber)) {
+                    s.add(p.landNumber);
+                    tempSum += p.landSize;
+                }
+            }
+            answer = Math.max(tempSum, answer);
+          
+        }
+  
+        return answer;
+    }
+    
+    public void bfs(int[][] land, int x, int y, int number){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+        int count = 1;
+        
+        List<int[]> path = new ArrayList<>();
+        path.add(new int[]{x,y});
+        
+        while(!q.isEmpty()){
+            int[] c = q.poll();
+            int cx = c[0];
+            int cy = c[1];
+            for(int d =0; d<4; d++){
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+                
+                if(0<=nx && nx<land.length && 0<=ny && ny <land[0].length && !visited[nx][ny] && land[nx][ny] == 1){
+                    count++;
+                    q.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    path.add(new int[]{nx,ny});
+                }
+            }
+        }
+        
+        for(int[] p : path){
+            newLand[p[0]][p[1]] = new Point(number, count);
+        }
+    }
 }
