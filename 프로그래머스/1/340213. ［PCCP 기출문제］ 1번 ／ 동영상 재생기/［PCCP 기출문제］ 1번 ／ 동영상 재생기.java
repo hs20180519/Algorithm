7c2974a8fed200eea97460f14400d282cd7a198e
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-06-08 10:45:05 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.07 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,54 +1,67 @@
+import java.util.*;
+import java.io.*;
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        // 현재 위치
-        int current = calculateTime(pos);
         
-        // 오프닝 시간
-        int opStart = calculateTime(op_start);
-        int opEnd = calculateTime(op_end);
+        int ctime = calTimeStrToInt(pos); // 현재 시간
+        int etime = calTimeStrToInt(video_len); // 종료 시간
+        int ostime = calTimeStrToInt(op_start); // 오프닝 시작 시간
+        int oetime = calTimeStrToInt(op_end); // 오프닝 종료 시간
         
-        for(String command : commands){   
-            // 만약 현재 위치가 오프닝 구간이라면, 오프닝 건너뛰기가 시행됨
-            if(opStart <= current && current <= opEnd){
-                current = opEnd;
-            }
- 
-            if(command.equals("prev")){
-                current -= 10;
-                if(current < 0){
-                    current = 0;
-                }
-            }else if(command.equals("next")){
-                current += 10;
-                int endTime = calculateTime(video_len);
-                if(current > endTime){
-                    current = endTime;
-                }
+        for(String c : commands){
+            if(ostime<= ctime && ctime < oetime){ // 오프닝 사이면 오프닝 끝 시간으로
+                ctime = oetime; 
             }
             
-            if(opStart <= current && current <= opEnd){
-                current = opEnd;
+            if(c.equals("next")){
+                ctime += 10;
+                ctime = ctime > etime ? etime : ctime;
+            }else{
+                ctime -= 10;
+                ctime = ctime < 0 ? 0 : ctime;
             }
             
         }
+        if(ostime<= ctime && ctime < oetime){ // 오프닝 사이면 오프닝 끝 시간으로
+            ctime = oetime; 
+        }
         
-        String answer = returnToString(current);
-       
-        return answer;
+        return calTimeIntToStr(ctime);
     }
     
-    public int calculateTime(String time){
-        return Integer.parseInt(String.valueOf(time.charAt(0)) +String.valueOf(time.charAt(1))) * 60 + Integer.parseInt(String.valueOf(time.charAt(3)) +String.valueOf(time.charAt(4)));
+    public int calTimeStrToInt(String t){ // String -> Int
+        char[] time = t.toCharArray();
+        int a = (time[0]-'0') * 10 + (time[1]-'0');
+        int b = (time[3]-'0') * 10 + (time[4]-'0');
+        return a*60 + b;
     }
     
-    public String returnToString(int time){
-        String a = String.valueOf(time/60);
-        String b = String.valueOf(time%60);
-        if(a.length()==1) a='0'+a;
-        if(Integer.parseInt(b)<10 && b.length()==1) b='0'+b;
-        if(Integer.parseInt(b)>=10 && b.length()==1) b=b+'0';
-        if(a.length()==0) a="00";
-        if(b.length()==0) b="00";
-        return a+":"+b;
+    public String calTimeIntToStr(int time){
+        int t = time/60;
+        int m = time%60;
+        StringBuilder sb = new StringBuilder();
+        
+        // 시간
+        if (t == 0){
+            sb.append("00");
+        }else if(1<= t && t< 10){
+            sb.append('0').append(t);
+        }else{
+            sb.append(t);
+        }
+        
+        sb.append(":");
+        
+        // 분
+        if (m == 0){
+            sb.append("00");
+        }else if(1<= m && m< 10){
+            sb.append('0').append(m);
+        }else{
+            sb.append(m);
+        }
+        
+        return sb.toString();
     }
+    
 }
