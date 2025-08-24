d987f094baef43c9636ac5f0c4c0dcfdbfc97d4f
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-08-24 10:52:29 +0900

    [level 3] Title: 입국심사, Time: 81.93 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,35 @@
 import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        // 모든 사람이 심사를 마치는 최소 시간은 1 ~ times최댓값 * n
+        // 시간에 따라 이분탐색하여 가능한 지 확인
         Arrays.sort(times);
-
-        long left = 1;
-        long right = (long) times[times.length - 1] * n; // 최악의 경우
-        long answer = right;
-
-        while (left <= right) {
-            long mid = (left + right) / 2;
-
-            long people = 0;
-            for (int time : times) {
-                people += mid / time;  // mid 시간 동안 몇 명 심사 가능?
-            }
-
-            if (people >= n) {
-                answer = mid;        // 가능하니까 시간 줄여보기
-                right = mid - 1;
-            } else {
-                left = mid + 1;      // 부족하니까 시간 늘리기
-            }
+        
+        long start = 0;
+        long end = times[times.length-1] * (long) n;
+        
+        while(start <= end){
+            long mid = (start + end) /2;
+            
+            if(isPossible(n, mid, times)){ // 가능한 경우, 더 적은 시간도 가능한지
+                answer = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }           
         }
-
         return answer;
+    }
+    
+    public boolean isPossible(int n, long mid, int[] times){
+        long people = 0; // 심사 가능한 사람
+        for(int time : times){
+            people += mid / time; // 걸린 시간 / 심사시간
+            if(people >= n) return true;
+        }
+        return false;
     }
 }
