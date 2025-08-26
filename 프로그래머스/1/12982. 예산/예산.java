a5b549f0a427bdad48495cb6f870e62f0df1093a
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-08-26 15:53:01 +0900

    [level 1] Title: 예산, Time: 0.59 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,16 +1,27 @@
 import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
+        int answer = 0;
+        int start = 0;
+        int end = d.length;
+        
         Arrays.sort(d);
-        int sum = 0;
-        int count = 0;
-        for(int i=0; i<d.length; i++){
-            if(sum+d[i] <= budget){
+        while(start<=end){
+            int mid = (start + end) / 2; // 예산
+            
+            int sum = 0; // 부서별 신청 금액 합
+            for(int i=0; i<mid; i++){
                 sum += d[i];
-                count++;
             }
+            
+            if(sum <= budget){ // 가능하다면
+                answer = Math.max(answer, mid); // 더 늘려보자
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+            
         }
-      
-        return count;
+        return answer;
     }
 }
