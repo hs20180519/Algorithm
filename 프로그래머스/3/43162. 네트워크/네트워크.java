5300fda8685a9aa27519bef824eb811580726a44
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-08-28 20:33:01 +0900

    [level 3] Title: 네트워크, Time: 1.00 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,58 @@
+import java.util.*;
+
 class Solution {
     boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n];
+        // 연결한 뒤에 bfs 돌리기
+        int gatsu = computers.length;
+        
+        visited = new boolean[gatsu];
+        List<Integer>[] arr = new ArrayList[gatsu];
+        
+        for(int i=0; i<gatsu; i++){
+            arr[i] = new ArrayList<>();
+        }
         for(int i=0; i<computers.length; i++){
-            if(!visited[i]){
-                dfs(i, computers);
-                answer++;
+            for(int j=0; j<computers.length; j++){
+                if(computers[i][j] == 1){
+                    arr[i].add(j);
+            
+                    // System.out.println(i+"랑"+j+"연결");
+                }
             }
         }
         
+       
+        // System.out.println(Arrays.toString(arr));
+        
+        
+        for(int i=0; i<gatsu; i++){
+            if(!visited[i]){
+                bfs(i, arr);
+                
+                // for(int j=0; j<gatsu; j++){
+                //     System.out.print(visited[j]+ " ");
+                // }
+                // System.out.println("");
+                answer++;
+            }
+        }
         return answer;
     }
-    void dfs(int node, int[][] computers){
-        visited[node] = true;
-        for(int i=0; i<computers.length; i++){
-            if(!visited[i] && computers[node][i] != 0){
-                dfs(i, computers);
+    public void bfs(int num, List<Integer>[] arr){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(num);
+        visited[num] = true;
+        while(!q.isEmpty()){
+            int curr = q.poll();
+    
+            for(int c : arr[curr]){
+                if(!visited[c]){
+                    
+                    visited[c] = true;   
+                    q.add(c);
+                }
             }
         }
     }
