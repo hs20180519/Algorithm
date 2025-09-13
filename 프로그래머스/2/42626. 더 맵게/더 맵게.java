c93fa908dfde7378cae3f30047055693b0d3a4a9
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-09-14 02:10:51 +0900

    [level 2] Title: 더 맵게, Time: 1467.25 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,24 +3,36 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int s : scoville){
-            pq.add(s);
-        }
-        while(pq.peek() < K){
-            // 1. poll 두번하고 pq에 삽입
-            if(pq.size() < 2){
-                return -1;
-            }
-            int first = pq.poll();
-            int second = pq.poll();
-            
-            int newNum = first + second*2;
-            answer++;
-            pq.add(newNum);
+        
+        // Arrays.sort(scoville);
+        int len = scoville.length;
+        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> a-b);
+        for(int i=0; i<len; i++){
+            q.add(scoville[i]);
         }
         
-        System.out.println(pq);
+        
+        
+        for(int i=0; i<len; i++){
+            while(q.size() >= 2){
+                int a = q.poll();
+                int b = q.poll();
+                if(a >= K && b >= K){
+                    break;
+                }
+                
+                if (a < K || b < K){
+                    q.add(a + (2*b));
+                    answer++;
+                     // System.out.println(q);
+                }
+            }
+        }
+        
+        while(!q.isEmpty()){
+            if(q.poll() < K) return -1;
+        }
+        
         return answer;
     }
 }
