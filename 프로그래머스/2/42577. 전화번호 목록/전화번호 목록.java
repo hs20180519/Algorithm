7bb763e44f68f146b3caab60d08fb2c27a9817aa
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-09-14 01:44:15 +0900

    [level 2] Title: 전화번호 목록, Time: 358.02 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,14 @@
 import java.util.*;
-
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        int len = phone_book.length;
         Arrays.sort(phone_book);
         
-        for(int i=0; i<phone_book.length-1; i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                return false;
-            }
+        for(int i=0; i<len-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
-     return true;
+       
+        return true;
     }
 }
