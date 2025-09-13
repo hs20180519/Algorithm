ff0c80547fe7d5b810ec87368143d8d6fd32e8b1
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-09-14 01:49:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.21 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,24 +1,20 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        // map 만들어서 각 이름마다 count +1
-        // completion 돌면서 count-1
-        // 마지막에 남아있는 이름이 답
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> hmap = new HashMap<>();
         for(String p : participant){
-            map.put(p, map.getOrDefault(p, 0)+1);
-        }
-    
-        for(String c : completion){
-            map.put(c, map.getOrDefault(c, 0)-1);
-        }
-        for(String s : map.keySet()){
-            if(map.get(s) == 1){
-                return s;
-            }
+            hmap.put(p, hmap.getOrDefault(p, 0) + 1);
         }
         
-        return answer;
+        for(String c : completion){
+            hmap.put(c, hmap.getOrDefault(c, 0) -1);
+        }
+        
+        for(String i : hmap.keySet()){
+            if(hmap.get(i) == 1){
+                return i;
+            }
+        }
+        return "";
     }
 }
