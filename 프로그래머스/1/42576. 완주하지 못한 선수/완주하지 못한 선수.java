7cbcfd176b2112ad3c41581c582aff78ee950f0b
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-09-27 01:14:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 106.60 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,21 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
         HashMap<String, Integer> hmap = new HashMap<>();
         for(String p : participant){
             hmap.put(p, hmap.getOrDefault(p, 0) + 1);
         }
         
         for(String c : completion){
-            hmap.put(c, hmap.getOrDefault(c, 0) -1);
+            hmap.put(c, hmap.getOrDefault(c, 0) - 1);
         }
         
-        for(String i : hmap.keySet()){
-            if(hmap.get(i) == 1){
-                return i;
-            }
+        for(String s : hmap.keySet()){
+            if(hmap.get(s) != 0) return s;
         }
-        return "";
+        
+        return answer;
     }
 }
