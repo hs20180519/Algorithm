c8490aaa036da9fd9b5cfe995d59fd710a2660cd
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-09-26 16:15:48 +0900

    [level 2] Title: 프로세스, Time: 1.67 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,42 +1,42 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        // 맨 앞을 빼고 맨 뒤에 넣어야 함 
-        // -> 이는 배열에서는 매우 비효율적, 큐 사용
+        Queue<int[]> q = new LinkedList<>();
         
-        // 1. Deque에 추가
-        Deque<Integer> c = new ArrayDeque<>(); 
-        for(int prior : priorities){
-            c.add(prior);
-        } 
+        for(int i=0; i<priorities.length; i++){
+            q.add(new int[] {i, priorities[i]});
+        }
         
-        // 2. 더 큰 수가 있는지 확인
-        int b = 0; // 제일 큰 수 일 때 0, 다른 큰 수가 있을 때 1
-        while(!c.isEmpty()){
-            for(int num : c){
-                if(num > c.getFirst()){ // 하나라도 큰 수가 있으면
-                    b = 1;
+        int count = 1;
+     
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            boolean skip = false;
+            // System.out.println("현재는"+ Arrays.toString(curr));
+            
+            for(int[] c : q){
+                if(c[1] > curr[1]){ // 더 높은 프로세스가 있다면
+                    // System.out.println("더 높은 프로세스가 있음");
+                    q.add(curr);
+                    skip = true;
                     break;
                 }
             }
-            if (b==1){ // 3-1. 하나라도 큰 수가 있으면
-                int n = c.removeFirst(); // 첫번째 수 빼서
-                c.add(n); // 뒤에 집어넣기
-                location = (location + c.size()-1)%c.size(); // loc = loc-1 (0인경우 가장 끝으로)
-                b = 0; // b 초기화
-            }
-            else{ // 3-2. 하나라도 큰 수가 없으면 (제일 큰 수라면)
-                c.removeFirst(); // 첫번째 수 빼기
-                if (location == 0){ // 찾는 위치가 처음 위치라면
-                    break; // 종료
+            // System.out.println();
+            
+           
+            if(!skip){
+                if(curr[0] == location){
+                    return count;
                 }
-                else{
-                    location = (location + c.size()-1)%c.size(); // loc = loc-1 (0인경우 가장 끝으로)
-                    answer++; // 다른 것의 호출 횟수
-                }     
+                count++;
             }
+            
         }
-        return answer+1;
+        
+        
+        return count;
     }
 }
