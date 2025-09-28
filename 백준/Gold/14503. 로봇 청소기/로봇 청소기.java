a5e95156c51eb2fdb7be4cf367ba1710a28d3825
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-09-28 10:43:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,110 +1,95 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N;
-	static int M;
-	static int d;
-	static int dx[] = { -1, 0, 1, 0 };// 북 동 남 서
-	static int dy[] = { 0, 1, 0, -1 };
-	static int[][] map;
-	static boolean[][] cleaned;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		cleaned = new boolean[N][M];
-
-		st = new StringTokenizer(br.readLine());
-		int startX = Integer.parseInt(st.nextToken()); // 현재 위치
-		int startY = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken()); // 현재 방향
-
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		clean(startX, startY);
-		
-		int ans =0;
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(cleaned[i][j] == true) {
-					ans++;
-				}
-			}
-		}
-		System.out.println(ans);
-	}
-	
-	public static void clean(int x, int y) {
-		
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {x, y});
-		
-		while(!q.isEmpty()) {
-			int[] curr =q.poll();
-			int cx = curr[0];
-			int cy = curr[1];
-			
-			if(map[cx][cy] == 0) {
-				cleaned[cx][cy] = true;
-			}
-			
-			// 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우
-			if(hasCleanSpaceAround(cx, cy)) {
-				for (int i = 0; i < 4; i++) {
-					turnLeft();
-					int nx = cx + dx[d];
-					int ny = cy + dy[d];
-
-					if (isInBound(nx, ny) && !(map[nx][ny] == 1) && !(cleaned[nx][ny])) {
-						q.add(new int[] {nx, ny});
-						break;
-					}
-				}
-			}
-			
-			// 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
-			// 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진
-			else{
-				int nx = cx - dx[d];
-				int ny = cy - dy[d];
-				if(!isInBound(nx, ny) || map[nx][ny] == 1) {
-					return;
-				}else {
-					q.add(new int[] {nx, ny});
-				}
-			}
-		}
-	}
-
-	public static boolean isInBound(int nx, int ny) {
-		return 0 <= nx && nx < N && 0 <= ny && ny < M;
-	}
-	
-	public static void turnLeft() {
-		d = (d+3)%4;
-	}
-	
-	public static boolean hasCleanSpaceAround(int x, int y) {
-		for(int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if(isInBound(nx, ny) && map[nx][ny]==0 && cleaned[nx][ny]==false) return true;
-		}
-		return false;
-	}
+    static int[] dx = {-1, 0, 1, 0}; // 위, 오른쪽, 아래, 왼쪽
+    static int[] dy = {0, 1, 0, -1};
+     public static void main(String[] args) throws IOException {
+      
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
+      
+      st = new StringTokenizer(br.readLine());
+      
+      int r = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
+      
+      int dir = Integer.parseInt(st.nextToken());
+      
+      int[][] room = new int[N][M];
+      boolean[][] visited = new boolean[N][M];
+      
+      for(int i=0; i<N; i++){
+        st = new StringTokenizer(br.readLine());
+        for(int j=0; j<M; j++){
+          room[i][j] = Integer.parseInt(st.nextToken());
+        }
+      }
+      
+      
+      while(true){
+        // System.out.println(r+ " " + c + " 현재 방향 "+ dir);
+        // 현재 칸이 아직 청소되지 않은 경우 현재 칸을 청소
+        if(room[r][c] == 0){
+          visited[r][c] = true;
+          room[r][c] = 2;
+        }
+          
+        // 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우
+        if(!isAroundClean(r,c, room)){
+          
+          dir--; // 반시계 회전
+          if(dir==-1) dir = 3;
+            
+          int nx = r+dx[dir];
+          int ny = c+dy[dir];
+            
+          // 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진
+          if(0<=nx && nx < N && 0<= ny && ny < M){
+            if(room[nx][ny] == 0){
+              r = nx;
+              c = ny;
+            }
+          }
+        }// 현재 칸의 주변 4칸 중 청소되지 않은 빈칸이 없는 경우
+        else{
+          // 한칸 후진
+          int nx = r-dx[dir];
+          int ny = c-dy[dir];
+            
+          // 벽이라 후진할 수 없다면 작동을 멈춘다
+          if(0<=nx && nx < N && 0<= ny && ny < M && room[nx][ny] != 1){
+            r = nx;
+            c = ny;
+          }else{
+            break;
+          }
+        }
+      }
+      
+      int answer = 0;
+      for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+          if(visited[i][j]) answer++;
+        }
+      }
+      System.out.println(answer);
+      
+     }
+     
+     static public boolean isAroundClean(int x, int y, int[][] room){
+       for(int d=0; d<4; d++){
+         int nx = x+dx[d];
+         int ny = y+dy[d];
+         if(0<=nx && 0<= ny && nx<room.length && ny<room[0].length){ // 범위 안
+            if(room[nx][ny] == 0) return false;
+         }
+       }
+       return true;
+    }
+      
+  
 }
