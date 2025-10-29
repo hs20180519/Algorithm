238805c5647e75b020d501639f1c8872821eba45
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-10-29 23:15:38 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
-SELECT B.BOOK_ID, A.AUTHOR_NAME, DATE_FORMAT(B.PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
-FROM BOOK AS B
-JOIN AUTHOR AS A
-ON B.AUTHOR_ID = A.AUTHOR_ID
-WHERE B.CATEGORY = '경제'
-ORDER BY B.PUBLISHED_DATE
+SELECT b.book_id, a.author_name, date_format(b.published_date, '%Y-%m-%d') as PUBLISHED_DATE
+from book b
+join author a
+on b.author_id = a.author_id
+where b.category = '경제'
+order by b.published_date
