4ad5d380d47f716c6c46af23e41384cc9caf2895
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-10-31 00:55:53 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 29.29 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,36 +1,35 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0; // 걸린 시간
-        int currentWeight = 0; // 현재 무게
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+        int time = 0;
+        int currentWeight = 0;
+        int idx = 0;
         
-        Queue <Integer> bridges = new LinkedList<>(); // 다리(를 지나고 있는 트럭)
-        Queue <Integer> trucks = new LinkedList<>(); // 트럭
-        
-        for(int truck : truck_weights){ // truck_weights -> trucks(queue)
-            trucks.add(truck);
+        for(int i=0; i<bridge_length; i++){ // 초기화
+            q.add(0);
         }
         
-        for(int i=0; i<bridge_length; i++){ // bridges를 bridge_length만큼 0으로 채움
-            bridges.add(0);
-        }
-        
-        while(!bridges.isEmpty()){
-            answer ++;
-            currentWeight -= bridges.poll(); // 트럭이 건너감
-            if (!trucks.isEmpty()) {
-                if (currentWeight + trucks.peek() <= weight) { // 다음 트럭이 다리에 올라올 수 있는지 확인
-                    int truck = trucks.poll();
-                    bridges.add(truck);
-                    currentWeight += truck;
+        while(!q.isEmpty()){
+            time++;
+            currentWeight -= q.poll();
+            
+             if (idx < truck_weights.length) {
+                if (currentWeight + truck_weights[idx] <= weight) {
+                    // 트럭 올리기
+                    q.add(truck_weights[idx]);
+                    currentWeight += truck_weights[idx];
+                    idx++;
                 } else {
-                    bridges.add(0); // 트럭이 올라올 수 없으면 0을 추가하여 시간이 지나도록 함
+                    // 올릴 수 없으면 0 추가 (시간만 흐르게)
+                    q.add(0);
                 }
             }
         }
         
-        return answer;
+        
+        return time;
     }
 }
