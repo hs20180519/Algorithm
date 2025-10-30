724e4ac9f363da333019007beb567bcf058fef5f
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-10-30 17:17:01 +0900

    [level 1] Title: 모의고사, Time: 0.80 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,49 +2,57 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = new int[3];
+
+        int[] a = {1,2,3,4,5}; // 5
+        int[] b = {2,1,2,3,2,4,2,5}; // 8
+        int[] c = {3,3,1,1,2,2,4,4,5,5}; // 10
         
-        int[] a = {1, 2, 3, 4, 5}; // 5
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5}; // 8
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10
+        int cntA = 0;
+        int cntB = 0;
+        int cntC = 0;
         
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == a[i%5]){
-                answer[0]++;
-            }
-            if(answers[i] == b[i%8]){
-                answer[1]++;
-            }
-            if(answers[i] == c[i%10]){
-                answer[2]++;
-            }
-            
+            if(answers[i] == a[i%5]) cntA++;
+            if(answers[i] == b[i%8]) cntB++;
+            if(answers[i] == c[i%10]) cntC++;
         }
-        int maxNum = Math.max(answer[0], Math.max(answer[1], answer[2]));
-            System.out.println(maxNum);
         
-        int[] newAnswer = new int[3];
+        int max = 0;
         int count = 0;
-        for(int i=0; i<3; i++){
-            if(answer[i] == maxNum){
-                newAnswer[count++] = i+1; 
-            }
+        if(max < cntA){
+            max = cntA;
+            count++;
         }
         
-        int lenNum = 0;
-        for(int i=0; i<3; i++){
-            if(newAnswer[i] != 0){
-                lenNum++;
-            }
+        if(max < cntB){
+            max = cntB;
+            count = 1;
+        }else if(max == cntB){
+            count++;
         }
-        int[] aa = new int[lenNum];
         
-        count = 0;
-        for(int i=0; i<3; i++){
-            if(newAnswer[i] != 0){
-                aa[count++] = newAnswer[i];
-            }
+        if(max < cntC){
+            max = cntC;
+            count = 1;
+        }else if(max == cntC){
+            count++;
         }
-        return aa;
+        
+        
+        int[] answer = new int[count];
+        int idx = 0;
+        
+        if(max == cntA){
+            answer[idx++] = 1;
+        }
+        if(max == cntB){
+            answer[idx++] = 2;
+        }
+        if(max == cntC){
+            answer[idx++] = 3;
+        }
+        
+        
+        return answer;
     }
 }
