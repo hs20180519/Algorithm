25ab0cb9a57acdfa71d003e6ae7235b26fc4737f
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-10-31 17:55:10 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 18332 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,51 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	/*
-	 * 중복있이 순열
-	 * 비내림차순 조건 추가
-	 */
-	static int N, M;
-	static StringBuilder sb;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		permute(0, new int[M]);
-		System.out.println(sb);
-	}
-	public static void permute(int cnt, int[] answers) {
-		// 증가하는 수열이 아니면 return
-		if(!isIncreasing(cnt, answers)) {
-			return;
-		}
-		
-		if(cnt == M) {
-			for(int i=0; i<cnt; i++) {
-				sb.append(answers[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=1; i<=N; i++) {
-			answers[cnt] = i;
-			permute(cnt+1, answers);
-		}
-		
-	}
-	
-	// 증가하는 수열이면 true
-	public static boolean isIncreasing(int cnt, int[] answers) {
-		for(int i =1; i<cnt; i++) {
-			if (answers[i-1] > answers[i]) return false;
-		}
-		return true;
-	}
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        dfs(0, M, N, new int[M]);
+        
+        System.out.println(sb.toString());
+        // 코드를 작성해주세요
+        
+    }
+    
+    public static void dfs(int depth, int M, int N, int[] temp){
+        if(!isIncreasing(depth, temp)){
+            // System.out.println(Arrays.toString(temp));
+            return;
+        }
+        
+        if(depth == M){
+            
+            for(int i:temp){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i=1; i<=N; i++){
+            temp[depth] = i;
+            dfs(depth+1, M, N, temp);
+        }
+    }
+    
+    public static boolean isIncreasing(int depth, int[] temp){
+        for(int i=1; i<depth; i++){
+            if(temp[i] < temp[i-1]) return false;
+        }
+        return true;
+    }
 }
