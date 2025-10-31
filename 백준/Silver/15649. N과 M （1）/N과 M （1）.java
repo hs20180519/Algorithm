c1b0bbb7eaedd4505aea201ab7872a77105a5506
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-10-31 17:17:44 +0900

    [Silver III] Title: N과 M (1), Time: 1564 ms, Memory: 51912 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N;
-    private static int M;
-    private static int[] answers;
-    private static boolean[] isSelected;
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); // N개의 자연수 중에
-        M = Integer.parseInt(st.nextToken()); // M개의 자연수 뽑음
-
-        answers = new int[M]; // M의 크기를 갖는 정답 리스트
-        isSelected = new boolean[N+1]; // N+1의 크기를 갖는 방문 여부 리스트
-        seq(0);
-    }
-    private static void seq(int cnt){
-        if (cnt == M){
-            for(int i=0; i<M; i++){
-                System.out.print(answers[i]+" ");
-            }
-            System.out.println("");
-        }
-        else{
-            for(int i=1; i<=N; i++){
-                if(isSelected[i]) continue; // 방문했으면 계속
-                answers[cnt] = i;
-                isSelected[i] = true;
-                seq(cnt+1);
-                isSelected[i] = false;
-
-            }
-        }
-    }
+	/*
+	 * 중복 없이 M개를 고른 순열
+	 */
+	static int N, M;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean[N+1];
+		permute(0, new int[M]);
+	}
+	public static void permute(int cnt, int[] answers) {
+		if(cnt == M) {
+			for(int i=0; i<cnt; i++) {
+				System.out.print(answers[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for(int i=1; i<=N; i++) {
+			if(visited[i])continue;
+			answers[cnt] = i;
+			visited[i] = true;
+			permute(cnt+1, answers);
+			visited[i] = false;
+		}
+	}
 }
