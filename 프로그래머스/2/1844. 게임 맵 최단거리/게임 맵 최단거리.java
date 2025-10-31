2f8cbd3b2386e123e8b706791be166d85432a050
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-11-01 00:46:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.97 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,53 @@
 import java.util.*;
+
 class Solution {
     int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
+    int[] dy = {0, -1, 0, 1};
     int[][] map;
+    boolean[][] visited;
+    int answer;
     public int solution(int[][] maps) {
-        int answer = 0;
+        answer = 0;
         map = maps;
-        // bfs(0,0);
-        return bfs(0,0);
+        visited = new boolean[map.length][map[0].length];
+        // System.out.println(map.length + " " + map[0].length);
+        
+        bfs(0, 0, map.length, map[0].length);
+        
+        return answer == 0 ? -1 : answer;
     }
     
-    public int bfs(int x, int y){
+    public void bfs(int x, int y, int n, int m){
         Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[map.length][map[0].length];
         q.add(new int[]{x, y, 1});
         visited[x][y] = true;
-        
         while(!q.isEmpty()){
             int[] curr = q.poll();
             int cx = curr[0];
             int cy = curr[1];
             int dist = curr[2];
             
-            if(cx == map.length-1 && cy == map[0].length-1){
-                return dist;
+            if(cx == n-1 && cy == m-1){
+                answer = dist;
+                break;
             }
             
             for(int d=0; d<4; d++){
                 int nx = cx+dx[d];
                 int ny = cy+dy[d];
-                if(0<=nx && nx < map.length && 0<=ny && ny<map[0].length && map[nx][ny] != 0 && !visited[nx][ny]){
-                    q.add(new int[]{nx, ny, dist+1});
+                
+                if(isOk(nx, ny) && !visited[nx][ny]){
                     visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny, dist+1});
                 }
             }
         }
-        return -1;
-        
+    }
+    
+    // 범위 안이고, 벽이 없을 때
+    public boolean isOk(int x, int y){
+        int n = map.length;
+        int m = map[0].length;
+        return 0 <= x && x< n && 0<= y && y < m && map[x][y] == 1;
     }
 }
