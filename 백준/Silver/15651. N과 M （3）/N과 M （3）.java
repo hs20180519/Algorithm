0eee3208373444922569dd2f0cf60a98ed51cdf7
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-10-31 17:43:01 +0900

    [Silver III] Title: N과 M (3), Time: 476 ms, Memory: 68788 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,36 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	/*
-	 * 중복 있이 M개를 고른 순열
-	 */
-	static int N, M;
-	static StringBuilder sb;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		permute(0, new int[M]);
-		System.out.println(sb);
-	}
-	public static void permute(int cnt, int[] answers) {
-		if(cnt == M) {
-			
-			for(int i=0; i<cnt; i++) {
-				sb.append(answers[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		for(int i=1; i<=N; i++) {
-			answers[cnt] = i;
-			permute(cnt+1, answers);
-		}
-	}
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        dfs(0, M, N, new int[M]);
+        
+        System.out.println(sb.toString());
+        // 코드를 작성해주세요
+        
+    }
+    
+    public static void dfs(int depth, int M, int N, int[] temp){
+        if(depth == M){
+            
+            for(int i:temp){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i=1; i<=N; i++){
+                temp[depth] = i;
+                dfs(depth+1, M, N, temp);
+        }
+    }
 }
