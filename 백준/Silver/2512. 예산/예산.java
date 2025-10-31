85758f834b38e799e9b7169b42036fe9f994ebef
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-11-01 01:10:40 +0900

    [Silver II] Title: 예산, Time: 152 ms, Memory: 15384 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -3,54 +3,52 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int N = Integer.parseInt(br.readLine());
-      int[] arr = new int[N];
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int sum = 0;
-      int maxNum = 0;
-      
-      // 일단 다 더한 다음에 M보다 작은지 확인
-      // 작으면 arr에서 가장 큰 값 찾아서 배출.
-      // 그렇지 않으면, 기준보다 작은 값들은 다 빼고, 나머지 큰 값 개수로 나눔
-      
-      for(int i=0; i<N; i++){
-        arr[i] = Integer.parseInt(st.nextToken());
-        sum+= arr[i];
-        maxNum = Math.max(arr[i], maxNum);
-      }
-      
-      
-      long M = Long.parseLong(br.readLine());
-      
-
-        if (sum <= M) {
-            System.out.println(maxNum);
-            return;
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int sum = 0;
+        int max = 0;
+        int answer = 0;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+            max = Math.max(arr[i], max);
         }
-
-        int left = 0;
-        int right = maxNum;
-        int result = 0;
-
-        while (left <= right) {
-            int mid = (left + right) / 2; // 상한
-            long total = 0;
-
-            for (int x : arr) {
-                total += Math.min(x, mid);
+        int M = Integer.parseInt(br.readLine());
+        
+        // 다 가능하다면 최댓값
+        if(sum <= M){
+            System.out.println(max);
+        }else{
+            int start = 0;
+            int end = max;
+            while(start <= end){
+                int mid = (start + end) / 2;
+                int mmsum = 0;
+                
+                for(int i=0; i<N; i++){
+                    if(arr[i] >= mid){
+                        mmsum += mid;
+                    }else{
+                        mmsum += arr[i];
+                    }
+                }
+                
+                if(mmsum <= M){
+                    answer = Math.max(mid, answer);
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
             }
-
-            if (total <= M) {
-                result = mid;
-                left = mid + 1;  // 더 높일 수 있는지 확인
-            } else {
-                right = mid - 1;
-            }
+            System.out.println(answer);
+            
+            
         }
-
-        System.out.println(result);
+        
+        
     }
-      
-  
 }
