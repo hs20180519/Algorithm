bfb89750d9f3655ff9316b851d20696db21ce3b6
hs20180519 <108255695+hs20180519@users.noreply.github.com>
2025-12-05 21:11:40 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 106.65 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,17 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        HashSet<Integer> set = new HashSet<>();
+        int answer = 0;
+        // 1 1 4 7 9 1 1 4 7 9 두배로 늘린 뒤, 5까지만 보기 슬라이딩 윈도우 크기 1, 2, 
         int n = elements.length;
-
+        int[] newElements = new int[2*n];
         for(int i=0; i<n; i++){
-            int sum = 0;
-            for(int j=0; j<n; j++){
-                sum += elements[(i+j)% n];
-                set.add(sum);
-            }
+            newElements[i] = elements[i];
+            newElements[i+n] = elements[i];
         }
-     
-        return set.size();
+        
+        HashSet<Integer> hset = new HashSet<>();
+        for(int i=1; i<=n; i++){ // 슬라이딩 윈도우 크기
+            int sum = 0;
+            for(int k=0; k<i; k++){
+                sum += newElements[k];
+            }
+            hset.add(sum);
+            
+            for(int j=0; j<n; j++){
+                sum += newElements[i+j] - newElements[j];  
+                hset.add(sum);
+            }
+
+        }
+
+        return hset.size();
     }
 }
